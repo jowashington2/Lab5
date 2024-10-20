843d838504af4098f5aa0ac65dc29efac8104619
Joshua Washington <jdwashington195@gmail.com>
2024-10-20 17:18:43 -0400

    Add files via upload

---
+++ ArticleSearch2-main/app/src/main/java/com/codepath/articlesearch/ArticleAdapter.kt
@@ -0,0 +1,61 @@
+package com.codepath.articlesearch
+
+import android.content.Context
+import android.content.Intent
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import android.widget.ImageView
+import android.widget.TextView
+import androidx.recyclerview.widget.RecyclerView
+import com.bumptech.glide.Glide
+
+const val ARTICLE_EXTRA = "ARTICLE_EXTRA"
+private const val TAG = "ArticleAdapter"
+
+class ArticleAdapter(private val context: Context, private val displayArticles: List<DisplayArticle>) :
+    RecyclerView.Adapter<ArticleAdapter.ViewHolder>() {
+
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): ViewHolder {
+        val view = LayoutInflater.from(context).inflate(R.layout.item_article, parent, false)
+        return ViewHolder(view)
+    }
+
+    override fun onBindViewHolder(holder: ViewHolder, position: Int) {
+        val displayArticle = displayArticles[position]
+        holder.bind(displayArticle)
+    }
+
+    override fun getItemCount() = displayArticles.size
+
+    inner class ViewHolder(itemView: View) : RecyclerView.ViewHolder(itemView),
+        View.OnClickListener {
+
+        private val mediaImageView = itemView.findViewById<ImageView>(R.id.mediaImage)
+        private val titleTextView = itemView.findViewById<TextView>(R.id.mediaTitle)
+        private val abstractTextView = itemView.findViewById<TextView>(R.id.mediaAbstract)
+
+        init {
+            itemView.setOnClickListener(this)
+        }
+
+        fun bind(displayArticle: DisplayArticle) {
+            titleTextView.text = displayArticle.headline
+            abstractTextView.text = displayArticle.abstract
+
+            Glide.with(context)
+                .load(displayArticle.mediaImageUrl)
+                .into(mediaImageView)
+        }
+
+        override fun onClick(v: View?) {
+            // Get selected article
+            val article = displayArticles[absoluteAdapterPosition]
+
+            // Navigate to Details screen and pass selected article
+            val intent = Intent(context, DetailActivity::class.java)
+            intent.putExtra(ARTICLE_EXTRA, article)
+            context.startActivity(intent)
+        }
+    }
+}
---
+++ ArticleSearch2-main/app/src/main/java/com/codepath/articlesearch/DetailActivity.kt
@@ -0,0 +1,38 @@
+package com.codepath.articlesearch
+
+import android.os.Bundle
+import android.widget.ImageView
+import android.widget.TextView
+import androidx.appcompat.app.AppCompatActivity
+import com.bumptech.glide.Glide
+
+private const val TAG = "DetailActivity"
+
+class DetailActivity : AppCompatActivity() {
+    private lateinit var mediaImageView: ImageView
+    private lateinit var titleTextView: TextView
+    private lateinit var bylineTextView: TextView
+    private lateinit var abstractTextView: TextView
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_detail)
+
+        mediaImageView = findViewById(R.id.mediaImage)
+        titleTextView = findViewById(R.id.mediaTitle)
+        bylineTextView = findViewById(R.id.mediaByline)
+        abstractTextView = findViewById(R.id.mediaAbstract)
+
+        val  displayArticle = intent.getSerializableExtra(ARTICLE_EXTRA) as DisplayArticle
+
+        // Set title and abstract information for the article
+        titleTextView.text =  displayArticle.headline
+        bylineTextView.text =  displayArticle.byline
+        abstractTextView.text =  displayArticle.abstract
+
+        // Load the media image
+        Glide.with(this)
+            .load( displayArticle.mediaImageUrl)
+            .into(mediaImageView)
+    }
+}
